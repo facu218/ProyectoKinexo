1592d179e7da98360718dc68498dbead10cb43a9
Facu218 <facucai_95@hotmail.com>
2018-11-20 22:36:49 -0300

    Creacion del proyecto

---
+++ ProyectoKinexo/ProyectoKinexo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProyectoKinexo.Startup))]
+namespace ProyectoKinexo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
